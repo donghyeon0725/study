e7f5d0d599c6a3387ac8e6ff70216b74731eff9e
김동현 <donghyeon@liveklass.com>
2022-03-29 23:37:16 +0900

    data structure

--- src/main/java/com/studyall/study/JavaLang.java
+++ src/main/java/com/studyall/study/JavaLang.java
@@ -1,7 +1,18 @@
 package com.studyall.study;
 
+import java.util.ArrayList;
+import java.util.Vector;
+
 public class JavaLang {
     public static void main(String[] args) {
+        System.out.println(System.nanoTime());
         System.out.println(System.getProperty("java.version"));
+        System.out.println(System.getenv("JAVA_HOME"));
+        System.out.println(System.nanoTime());
+
+        // thread safe
+        Vector vector = new Vector();
+        // non thread safe
+        ArrayList arrayList = new ArrayList();
     }
 }
