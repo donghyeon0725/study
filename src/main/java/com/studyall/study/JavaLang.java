c7452700e64af79d2102b2738dd70c1fefca842d
김동현 <donghyeon@liveklass.com>
2022-03-30 01:09:39 +0900

    data structure

--- src/main/java/com/studyall/study/JavaLang.java
+++ src/main/java/com/studyall/study/JavaLang.java
@@ -1,7 +1,6 @@
 package com.studyall.study;
 
-import java.util.ArrayList;
-import java.util.Vector;
+import java.util.*;
 
 public class JavaLang {
     public static void main(String[] args) {
@@ -14,5 +13,41 @@ public class JavaLang {
         Vector vector = new Vector();
         // non thread safe
         ArrayList arrayList = new ArrayList();
+
+        run();
+    }
+
+    private static void run() {
+
+        System.out.println("=========");
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("string1");
+        list.add("string2");
+        list.add("string3");
+
+        String[] array = new String[2];
+        String[] strings = list.toArray(array);
+
+        for (String s : strings) {
+            System.out.println(s);
+        }
+
+        List<Object> objects = Collections.synchronizedList(new ArrayList<>());
+
+        Stack stack = new Stack();
+
+        Collection collections = null;
+
+        for (Object o : collections) {
+
+        }
+
+        HashSet<String> set = new HashSet();
+
+        for (String s : set) {
+            System.out.println(s);
+        }
     }
 }
