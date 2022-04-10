c5a70fdaaf1f2f138346037c411f38e58beecfc3
김동현 <donghyeon@liveklass.com>
2022-04-10 23:19:19 +0900

    latch

---
+++ src/main/java/com/studyall/study/concurrency/LatchSample.java
@@ -0,0 +1,44 @@
+package com.studyall.study.concurrency;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class LatchSample {
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(5);
+
+        ExecutorService pool = Executors.newFixedThreadPool(5);
+
+        for (int i=0; i<5; i++) {
+            pool.submit(new Thread(() -> {
+                System.out.println(Thread.currentThread().getName() + " Done API Call");
+
+                try {
+                    latch.countDown();
+                    latch.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println(Thread.currentThread().getName() + " Continue processing");
+            }));
+        }
+
+        System.out.println(Thread.currentThread().getName() + " about to await on main");
+        latch.await();
+        System.out.println(Thread.currentThread().getName() + " done awaiting on main..");
+
+        pool.shutdown();
+
+        try {
+            pool.awaitTermination(5, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("API Processing Complete");
+    }
+
+}
