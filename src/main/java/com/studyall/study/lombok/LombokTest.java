c5329a90350d361d79b8d080d312686af2b6cb48
김동현 <donghyeon@liveklass.com>
2022-04-28 23:06:41 +0900

    lombok test

---
+++ src/main/java/com/studyall/study/lombok/LombokTest.java
@@ -0,0 +1,8 @@
+package com.studyall.study.lombok;
+
+public class LombokTest {
+    public static void main(String[] args) {
+        Manager manager = new Manager();
+        manager.setAge(2);
+    }
+}
---
+++ src/main/java/com/studyall/study/lombok/Manager.java
@@ -0,0 +1,10 @@
+package com.studyall.study.lombok;
+
+import lombok.Data;
+
+@Data
+public class Manager {
+    private String uuid;
+    private String name;
+    private Integer age;
+}
