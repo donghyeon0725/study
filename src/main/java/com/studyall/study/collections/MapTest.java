42fb69eb2b469c0272588def2239b3dffe6c824e
김동현 <donghyeon@liveklass.com>
2022-05-01 22:38:26 +0900

    map test

---
+++ src/main/java/com/studyall/study/collections/MapTest.java
@@ -0,0 +1,10 @@
+package com.studyall.study.collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapTest {
+    public static void main(String[] args) {
+        Map<String, Object> map = new HashMap<>();
+    }
+}
