e0c86e884e8e8cac31f17af00a2d1081823f9b5d
김동현 <donghyeon@liveklass.com>
2022-05-06 16:14:16 +0900

    float

--- src/main/java/com/studyall/study/collections/MapTest.java
+++ src/main/java/com/studyall/study/collections/MapTest.java
@@ -6,5 +6,6 @@ import java.util.Map;
 public class MapTest {
     public static void main(String[] args) {
         Map<String, Object> map = new HashMap<>();
+        map.put("key", "test");
     }
 }
---
+++ src/main/java/com/studyall/study/floatTest/Test.java
@@ -0,0 +1,13 @@
+package com.studyall.study.floatTest;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(1 - 0.1d * 7);
+
+        float num3 = 1.23456789f;
+        double num4 = 1.23456789;
+
+        System.out.println("float형  변수 num3 : " + num3);
+        System.out.println("double형 변수 num4 : " + num4);
+    }
+}
